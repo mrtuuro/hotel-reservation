61b34bd9f93018b411d39b0aae1e85b02b6957c5
mrtuuro <t.ozay98@gmail.com>
2023-06-07 17:15:31 +0300

    added user_handler_and user_store and database layer (video 2)

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,18 +1,36 @@
 package api
 
 import (
+	"context"
 	"github.com/gofiber/fiber/v2"
+	"github.com/mrtuuro/hotel-reservation/db"
 	"github.com/mrtuuro/hotel-reservation/types"
 )
 
-func HandleGetUsers(c *fiber.Ctx) error {
+type UserHandler struct {
+	userStore db.UserStore
+}
+
+func NewUserHandler(userStore db.UserStore) *UserHandler {
+	return &UserHandler{userStore: userStore}
+}
+
+func (u *UserHandler) HandleGetUser(c *fiber.Ctx) error {
+	var (
+		id  = c.Params("id")
+		ctx = context.Background()
+	)
+	user, err := u.userStore.GetUserByID(ctx, id)
+	if err != nil {
+		return err
+	}
+	return c.JSON(user)
+}
+
+func (u *UserHandler) HandleGetUsers(c *fiber.Ctx) error {
 	user := types.User{
 		FirstName: "Jon",
 		LastName:  "Waldo",
 	}
 	return c.JSON(user)
-}
-
-func HandleGetUserById(c *fiber.Ctx) error {
-	return c.JSON("one user returned")
 }
--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"github.com/gofiber/fiber/v2"
 	"github.com/mrtuuro/hotel-reservation/api"
+	"github.com/mrtuuro/hotel-reservation/db"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 )
 
+const dburi = "mongodb://localhost:27017"
+const dbName = "hotel-reservation"
+const usercoll = "users"
+
 func main() {
+	var config = fiber.Config{
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			return ctx.JSON(map[string]string{"error": err.Error()})
+		},
+	}
+
 	listenAddr := flag.String("listenAddr", ":3000", "Server's port")
 	flag.Parse()
-	app := fiber.New()
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// handler initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
+
+	app := fiber.New(config)
 	v1 := app.Group("/api/v1")
 
-	v1.Get("/user", api.HandleGetUsers)
-	v1.Get("/user/:id", api.HandleGetUserById)
+	v1.Get("/user", userHandler.HandleGetUsers)
+	v1.Get("/user/:id", userHandler.HandleGetUser)
 
 	log.Fatal(app.Listen(*listenAddr))
 
